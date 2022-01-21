e3c2fe9cd406aa1591b3d2e5f4c3547786b9eb94
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-01-20 23:27:17 -0300

    feat: addition of the first entities

--- src/profile/entities/profile.entity.ts
+++ src/profile/entities/profile.entity.ts
@@ -1,1 +1,28 @@
-export class Profile {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'profile' })
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  photo: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  country: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: number;
+
+  @Column()
+  email: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column({ name: 'is_active' })
+  isActive: boolean;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  cpf: string;
+}
